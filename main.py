0e9b1680c0b787b749dcd7cc2050fb587e2fec49
BJ-dev0706 <kitchi734@gmail.com>
2024-12-02 03:29:05 -0500

    fix: appdata folder move

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from database import init_db
+from appdata.database import init_db
 from password_manager_app import PasswordManagerApp
 
-# Initialize the database
 init_db()
 
-# Start the GUI application
 if __name__ == "__main__":
     app = PasswordManagerApp()
     app.check_update(False)
